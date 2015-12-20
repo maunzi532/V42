3a2fe0712d005f9548afb43ab927c623b3b488dc
maunzi532 <maunzi532@bitbucket.org>
2015-12-20 11:32:41 +0100

    PYRAMIDE CONFRMED

--- src/nonBlock/aussehen/LadeTeil.java
+++ src/nonBlock/aussehen/LadeTeil.java
@@ -72,8 +72,11 @@ public class LadeTeil
 				else if(zeilen[i].startsWith("PRISMA"))
 				{
 					String[] x2 = zeilen[i].split(" ");
-					//noinspection ConstantConditions
-					Boolean seite = x2[0].endsWith("+") ? true : x2[0].endsWith("-") ? false : null;
+					Boolean seite = null;
+					if(x2[0].endsWith("+"))
+						seite = true;
+					else if(x2[0].endsWith("-"))
+						seite = false;
 					int seitig = Integer.parseInt(x2[1]);
 					int[][] daten = new int[seitig][6];
 					int sh = 2;
@@ -109,6 +112,11 @@ public class LadeTeil
 						LadeF2 l;
 						if(xfall != null)
 							l = new LadeF2(xfall, seite);
+						else if(x2[sh].equals("NICHT"))
+						{
+							sh++;
+							continue;
+						}
 						else if(x2[sh].endsWith("-ALL"))
 						{
 							xfall = XFarbe.t2xf(x2[sh].substring(0, x2[sh].length() - 4));
@@ -121,6 +129,90 @@ public class LadeTeil
 							l.ecken1.add(daten[(j + (m == 1 || m == 2 ? 1 : 0)) % seitig][m > 1 ? 3 : 0]);
 							l.ecken2.add(daten[(j + (m == 1 || m == 2 ? 1 : 0)) % seitig][m > 1 ? 4 : 1]);
 							l.ecken3.add(daten[(j + (m == 1 || m == 2 ? 1 : 0)) % seitig][m > 1 ? 5 : 2] == 1);
+						}
+						f2.add(l);
+						sh++;
+					}
+				}
+				else if(zeilen[i].startsWith("PYRAMIDE"))
+				{
+					String[] x2 = zeilen[i].split(" ");
+					Boolean seite = null;
+					if(x2[0].endsWith("+"))
+						seite = true;
+					else if(x2[0].endsWith("-"))
+						seite = false;
+					int seitig = Integer.parseInt(x2[1]);
+					int[][] daten = new int[seitig][3];
+					int ecke1;
+					boolean ecke3 = false;
+					String[] cxe2 = x2[2].split("-");
+					if(cxe2[0].startsWith("+"))
+					{
+						ecke1 = Integer.parseInt(cxe2[0].substring(1));
+						ecke3 = true;
+					}
+					else
+						ecke1 = Integer.parseInt(cxe2[0]);
+					int ecke2 = Integer.parseInt(cxe2[1]);
+					int sh = 3;
+					int all = -1;
+					for(int j = 0; j < seitig; j++)
+					{
+						if(all < 0)
+						{
+							String[] cx2 = x2[sh].split("-");
+							if(cx2.length > 2 && cx2[2].equals("ALL"))
+								all = j;
+							if(cx2[0].startsWith("+"))
+							{
+								daten[j][0] = Integer.parseInt(cx2[0].substring(1));
+								daten[j][2] = 1;
+							}
+							else
+								daten[j][0] = Integer.parseInt(cx2[0]);
+							daten[j][1] = Integer.parseInt(cx2[1]);
+							sh++;
+						}
+						else
+						{
+							daten[j][0] = daten[all][0];
+							daten[j][1] = daten[all][1] + j - all;
+							daten[j][2] = daten[all][2];
+						}
+					}
+					XFarbe xfall = null;
+					for(int j = 0; j < seitig; j++)
+					{
+						LadeF2 l;
+						if(xfall != null)
+							l = new LadeF2(xfall, seite);
+						else if(x2[sh].equals("NICHT"))
+						{
+							sh++;
+							continue;
+						}
+						else if(x2[sh].endsWith("-ALL"))
+						{
+							xfall = XFarbe.t2xf(x2[sh].substring(0, x2[sh].length() - 4));
+							l = new LadeF2(xfall, seite);
+						}
+						else
+							l = new LadeF2(XFarbe.t2xf(x2[sh]), seite);
+						for(int m = 0; m < 3; m++)
+						{
+							if(m != 2)
+							{
+								l.ecken1.add(daten[(j + m) % seitig][0]);
+								l.ecken2.add(daten[(j + m) % seitig][1]);
+								l.ecken3.add(daten[(j + m) % seitig][2] == 1);
+							}
+							else
+							{
+								l.ecken1.add(ecke1);
+								l.ecken2.add(ecke2);
+								l.ecken3.add(ecke3);
+							}
 						}
 						f2.add(l);
 						sh++;
